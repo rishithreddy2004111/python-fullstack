ad2829d9255d266538c4f52bb14ab6975077787b
rishithreddy2004111 <rishith197@gmail.com>
2025-08-23 10:30:45 +0530

    Update 6.py

--- list-comprehension/6.py
+++ list-comprehension/6.py
@@ -1,3 +1,4 @@
+# From a list nums = [1, 2, 3, 4, 5], create a new list of tuples where each tuple contains the number and its cube. Example: [(1,1), (2,8), (3,27), ...].
 n=[1,2,3,4,5]
 arr=[(i,i**3) for i in n]
 print(arr)
