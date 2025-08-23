fe750f7593526406fa57eb1339f0412c1c77deed
rishithreddy2004111 <rishith197@gmail.com>
2025-08-23 10:31:11 +0530

    Update 7.py

--- list-comprehension/7.py
+++ list-comprehension/7.py
@@ -1,3 +1,4 @@
+# Given a sentence: "List comprehensions are powerful", extract all words that have more than 5 letters.
 s="List comprehensions are powerful"
 s1=s.split()
 arr=[i for i in s1 if len(i)>=5]
