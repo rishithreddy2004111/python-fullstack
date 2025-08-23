c7654c9e2a2d4b4a31942564d642ac4c4346e597
rishithreddy2004111 <rishith197@gmail.com>
2025-08-23 10:32:47 +0530

    Update 10.py

--- list-comprehension/10.py
+++ list-comprehension/10.py
@@ -1,3 +1,4 @@
+# From a list words = ["madam", "python", "level", "world"], extract only the palindromes using list comprehension.
 s=["madam", "python", "level", "world"]
 s1=[i for i in s if i==i[::-1]]
 print(s1)
