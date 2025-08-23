45a546aeba07d52623ff005e945f11bb047822ff
rishithreddy2004111 <rishith197@gmail.com>
2025-08-23 10:40:56 +0530

    08.py

--- list-comprehension/08.py
+++ list-comprehension/08.py
@@ -1,4 +1,4 @@
-# Flatten a 2D list matrix = [[1,2],[3,4],[5,6]] into a single list [1,2,3,4,5,6] using list comprehension.
+# Flatten a 2D list matrix = [[1,2],[3,4],[5,6]] into a single list [1,2,3,4,5,6] using list comprehension. 
 arr=[[1,2],[3,4],[5,6]]
 a=[arr[i][j] for i in range(len(arr)) for j in range(len(arr[0]))]
 print(a)
