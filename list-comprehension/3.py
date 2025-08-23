963cd0833da05e4c90c0c414cac636bcd66fefcb
rishithreddy2004111 <rishith197@gmail.com>
2025-08-23 10:28:22 +0530

    Update 3.py

--- list-comprehension/3.py
+++ list-comprehension/3.py
@@ -1,3 +1,4 @@
+# Given a list words = ["apple", "bat", "car", "dog"], use list comprehension to create a new list with the first letter of each word.
 a=["apple", "bat", "car", "dog"]
 arr=[i[0] for i in a]
 print(arr)
