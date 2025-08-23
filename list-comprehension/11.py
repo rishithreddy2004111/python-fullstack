744bb76e4998884b0a6a63864e32be5fc3b21b13
rishithreddy2004111 <rishith197@gmail.com>
2025-08-23 10:33:32 +0530

    11.py

--- list-comprehension/11.py
+++ list-comprehension/11.py
@@ -1,3 +1,4 @@
+# Given a dictionary students = {"John": 85, "Alice": 92, "Bob": 76, "Daisy": 89}, use list comprehension to get names of students who scored more than 80.
 s = {"John": 85, "Alice": 92, "Bob": 76, "Daisy": 89}
 arr=[key for key,i in s.items() if i>80 ]
 print(arr)
